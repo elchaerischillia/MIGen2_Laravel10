16a8cf1c537c2f3df87185fa251a52489a4329c1
elchaerischillia <elchaerischillia@gmail.com>
2024-04-17 08:35:59 +0700

    First Commit

---
+++ database/migrations/2024_03_18_075751_create_detail_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id(); 
+            $table->foreignId('type_user_id') ->nullable()-> index ('fk_detail_user_to_type_user'); 
+            $table->foreignId('type_user_id') ->nullable()->index('fk_detail_user_to_type_user');
+            $table->string('contact')->nullable(); 
+            $table->longtext('address')->nullable(); 
+            $table->longtext('photo')->nullable(); 
+            $table->enum('gender', [1,2])->nullable(); 
+            $table->timestamps(); 
+            $table->softdeletes(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_user');
+    }
+};
